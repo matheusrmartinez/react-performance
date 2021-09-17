aeda3d0b3350841ba9cc3f05f132015f4bce0204
matheus.martinez <matheus.martinez@uppertools.inf.br>
2021-09-17 08:00:17 -0300

    Criação estrutura inicial

---
+++ components/SearchResults.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ProductItem } from "./ProductItem";
+
+interface SearchResultsProps {
+  results: {
+    id: number;
+    price: number;
+    title: string;
+  }[];
+}
+
+export function SearchResults({ results }: SearchResultsProps) {
+  return (
+    <div>
+      {results?.map((product) => {
+        return <ProductItem product={product} key={product.id} />;
+      })}
+    </div>
+  );
+}
